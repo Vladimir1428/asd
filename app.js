3702331b468a8c12de096fde46460a8acf3b6614
Vladimir1428 <vova123555@gmail.com>
2023-12-31 14:57:02 +0200

    as

--- app.js
+++ app.js
@@ -1,33 +1,24 @@
 const WebSocket = require('ws');
-const http = require('http');
 
-// Создаем HTTP сервер
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('WebSocket Server is running');
-});
-
-
-
-// Устанавливаем соединение с "wss://ws.dextools.io/"
-const dextoolsSocket = new WebSocket('wss://ws.dextools.io/', {
-    headers: {
-      'Accept-Encoding': 'gzip, deflate, br',
-      'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
-      'Cache-Control': 'no-cache',
-      'Connection': 'Upgrade',
-      'Host': 'ws.dextools.io',
-      'Origin': 'https://nodejsapp-ia4p.onrender.com',
-      'Pragma': 'no-cache',
-      'Sec-Websocket-Version': '13',
-      'Upgrade': 'websocket',
-    }
+const ws = new WebSocket('wss://ws.dextools.io/' {
+    origin: 'https://nodejsapp-ia4p.onrender.com' // Укажите свой домен вместо https://your-origin.com
   });
 
-// Обработчик события при успешном открытии соединения с "wss://ws.dextools.io/"
-dextoolsSocket.on('open', () => {
-  console.log('Соединение с "wss://ws.dextools.io/" установлено.');
+ws.on('open', () => {
+  console.log('Соединение установлено.');
+
+  // Теперь вы можете отправлять сообщения на сервер.
+  ws.send('Привет, сервер!');
 });
-dextoolsSocket.on('error', (error) => {
-    console.error('Ошибка при установке соединения с Dextools:', error.message);
-  });
+
+ws.on('message', (data) => {
+  console.log(`Получено сообщение: ${data}`);
+});
+
+ws.on('close', () => {
+  console.log('Соединение закрыто.');
+});
+
+ws.on('error', (error) => {
+  console.error(`Ошибка: ${error.message}`);
+});
